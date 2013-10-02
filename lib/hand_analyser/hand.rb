97832ded5d480894973c795b273ee70c46e3663e
Tom Russell <tom.russell@initforthe.com>
2013-10-02 19:33:51 +0100

    Write initializer method to prevent error #1 from test

--- lib/hand_analyser/hand.rb
+++ lib/hand_analyser/hand.rb
@@ -1,4 +1,15 @@
 module HandAnalyser
   class Hand
+    def initialize(hand_string)
+      @cards = cards_from(hand_string)
+    end
+
+    private
+
+    def cards_from(str)
+      cards_arr = str.split(' ')
+      # Remove suits from each
+      cards_arr.map { |c| c[0] }
+    end
   end
 end
